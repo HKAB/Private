4e5c255d6ccc97cba3a7966c6515e8157868cac5
Nguyễn Phú Trường <khoahockithuatmc@gmail.com>
2019-06-15 19:30:31 +0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,4 +1,3 @@
-$('#leaderboard > table > tbody > tr:nth-child(14)').remove();
 $('#leaderboard > table > tbody > tr > td.center > a').each(function(index) {$(this).text('hkab');});
 $('#leaderboard > table > tbody > tr > td.center > img').attr('src', 'https://img.icons8.com/doodle/48/000000/hospital-2.png');
 $('#showanswer > div.center').append('<img src="https://scontent.fhan4-1.fna.fbcdn.net/v/t1.15752-9/59932784_331166140923914_9169254980773740544_n.jpg?_nc_cat=104&_nc_oc=AQlA1batjlZHfIfBHaTnD_Sdincbs3Y0FMIsp4TmZQxdVdWIzHptE4oe4aLSWSePwoQMk0zpHMfDih_eNweWnbqC&_nc_ht=scontent.fhan4-1.fna&oh=1e725bb71bc62b990ddfa071e142299e&oe=5D80FEEB" width=500 height=500></img>');
